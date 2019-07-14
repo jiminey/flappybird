964606b79eca9e3aede3623ac9fdf35ca6b0f08e
jiminey <jtwnguyen@ucdavis.edu>
2019-07-14 16:16:37 -0700

    Add bg constant

--- Original-Flappy-bird-JavaScript-master/game.js
+++ Original-Flappy-bird-JavaScript-master/game.js
@@ -7,16 +7,53 @@ const ctx = cvs.getContext('2d');
 //game vars and consts 
 
 let frames = 0; 
+    //looping will increment frames 
+
+
+
+//load sprite image
+const sprite = new Image(); 
+sprite.src = 'img/sprite.png'
+
+
+//background 
+const bg = {
+    sX: 0, 
+    sY: 0,
+    w: 275, 
+    h: 226,
+    x: 0, 
+    y: cvs.height - 226,
+
+
+    draw : function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this,y, this.w, this.h); 
+    }
+    // drawimage takes in (sprite, sx,sy ,swidth, sheight, dx, dy, dw,dh )
+    // s = src image, and d = destination canvas
+}   
 
 //draw 
 
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw(); // draw bg
 }
 
 //update 
 
 function update() {
+}
 
-}g
+//loop
+
+function loop() {
+    update();
+    draw(); 
+    frames++; 
+
+    requestAnimationFrame(loop); 
+    //this rerenders the page so bird and pipe moves 
+}
