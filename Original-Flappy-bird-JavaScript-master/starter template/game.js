e43b80cebf8d444c1cdb24a2da3813f0e8682ac8
jiminey <jtwnguyen@ucdavis.edu>
2019-07-15 08:31:48 -0700

    Game over message

--- Original-Flappy-bird-JavaScript-master/starter template/game.js
+++ Original-Flappy-bird-JavaScript-master/starter template/game.js
@@ -67,6 +67,23 @@ const getReady = {
 
 }
 
+
+const gameOver = {
+    sX: 175,
+    sY: 228,
+    w: 225,
+    h: 202,
+    x: cvs.width / 2 - 225 / 2,
+    y: 90,
+
+    draw: function () {
+        if (state.current == state.over) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+    }
+
+}
+
 const bird = {
     animation : [
         { sX: 276, sY: 112 },
